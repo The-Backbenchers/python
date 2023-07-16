b102d72602503c843c3170db7d0d016d8a62beeb
Syed Saif Ahmed <syedsaifahmed55@gmail.com>
2023-07-16 17:47:27 +0530

    Python code

---
+++ list_function.py
@@ -0,0 +1,19 @@
+#append list.append(element)
+
+list1 = [2,6,7,9,6]
+#insert function
+#list1.insert(1,99)
+#extend
+#list1.extend([4,78])
+# remove 
+#list1.remove(6)
+#pop
+#list1.pop()
+# copy
+#list2 = list1.copy()
+# count
+#sort 
+#list1.sort()
+#reverse
+list1.reverse()
+print(list1)
---
+++ list_practice.py
@@ -0,0 +1,31 @@
+# Create a list in python 
+
+list1 = [2,4,5,7]
+list2 = list((6,9,8))
+
+#print(list1)
+#print(list2)
+
+#Fun fact 
+
+list3 = list("14567")
+#print(list3)
+
+# Indexing 
+
+list4 = [3,6,7,9]
+list4[1] = 10
+#print(list4)
+
+# heterogenious
+list5 = [1,4,5,9.1,8.7,"Hello","World",(5,87,9),{7,9}]
+#print(list5)
+# Duplicate 
+list6 = [1,1,4,4,4,8]
+#print(list6)
+
+# list slicing LIST[start:ending - 1:incriment]
+#print(list5[1:6:2])
+
+# concatination
+print(list5 + list6)
